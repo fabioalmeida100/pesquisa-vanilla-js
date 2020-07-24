c0ebae5bc183c03078479ea6ac35d01eff63518c
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-07-24 02:39:31 -0300

    Rendering panel of the people

--- front-end/assets/js/script.js
+++ front-end/assets/js/script.js
@@ -7,12 +7,21 @@ let statisticsPanel = null;
 let buttonSearch = null;
 let inputSearch = null;
 let numberFormat = Intl.NumberFormat('pt-BR');
+let panelNoPeople = null;
+let panelNoStatistics = null;
+let peoplePanelDiv = null;
+let statisticsPanelDiv = null;
+let countResult = 0;
 
 window.addEventListener('load', () => {
   peoplePanel = document.querySelector('#people-panel');
   statisticsPanel = document.querySelector('#statistics-panel');
   buttonSearch = document.querySelector('#search').addEventListener('click', searchPeople);
   inputSearch = document.querySelector('#term-search');
+  panelNoPeople = document.querySelector('#panel-no-people');
+  panelNoStatistics = document.querySelector('#panel-no-statistics');
+  peoplePanelDiv = document.querySelector('#people-panel');
+  statisticsPanelDiv = document.querySelector('#statistics-panel');
   inputSearch.focus();
   loadPeople();
 })
@@ -39,14 +48,16 @@ function searchPeople() {
 
 function doListPeoples() {
   _peoplesFilterGlobal = _peoplesGlobal.filter((item) => {
-    return item.name.toLowerCase().includes(inputSearch.value);
+    return item.name.toLowerCase().includes(inputSearch.value.toLowerCase());
+  }).sort((a, b) => {
+    return a.name.localeCompare(b.name);
   })
-  console.log(_peoplesFilterGlobal);
 }
 
 function doStatistics() {
   let maleCount = 0;
   let femaleCount = 0;
+  countResult = _peoplesFilterGlobal.length;
 
   let agesTotal = _peoplesFilterGlobal.reduce((accumulator, current) => {
     return accumulator + current.age
@@ -69,5 +80,33 @@ function doStatistics() {
 }
 
 function render() {
+  handleShowNoResult();
+  renderPeoplePanel();
+}
 
+function handleShowNoResult() {
+  if (countResult > 0) {    
+    panelNoPeople.classList.add('d-none');
+    panelNoStatistics.classList.add('d-none');
+  } else {
+    panelNoPeople.classList.remove('d-none');
+    panelNoStatistics.classList.remove('d-none');
+  }
+}
+
+function renderPeoplePanel() {
+  if (countResult > 0) {
+    peoplePanelDiv.innerHTML = `<h4 class="m-4"><span> ${ countResult } </span> usuário(s) encontrado(s)</h4>`;
+    _peoplesFilterGlobal.forEach((item, i) => {
+        let div = `
+            <div class="col-12 mb-2">
+              <img class="rounded-circle mr-2" src="${ item.picture }" /> ${ item.name }, ${ item.age }  anos
+            </div>
+        `
+        peoplePanelDiv.innerHTML = peoplePanelDiv.innerHTML + div;
+    });
+  } else {
+    peoplePanelDiv.innerHTML = '';
+    statisticsPanelDiv.innerHTML = '';
+  }
 }
